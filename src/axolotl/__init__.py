dcbde916680116057ec4d845d9beb302b93b87f6
xcr <xcr@xcr>
2026-06-23 22:38:37 +0800

    Kusanagi238__diff__id_479

--- src/axolotl/__init__.py
+++ src/axolotl/__init__.py
@@ -4,4 +4,12 @@ import pkgutil
 
 __path__ = pkgutil.extend_path(__path__, __name__)  # Make this a namespace package
 
-__version__ = "0.12.0.dev"
+if "__version__" not in globals():
+    try:
+        from importlib.metadata import version as _get_version
+    except Exception:
+        try:
+            from importlib_metadata import version as _get_version
+        except Exception:
+            _get_version = None
+    __version__ = _get_version("axolotl") if _get_version else None
